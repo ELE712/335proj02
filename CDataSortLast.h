ff204614e83a52e7be28487e07a516a716795ddc
ELE712 <zandber2@msu.edu>
2016-02-28 12:24:10 -0500

    Updated Header

--- CDataSortLast.h
+++ CDataSortLast.h
@@ -1,15 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * File:   CDataSortLast.h
- * Author: zekezandbergen
- *
- * Created on February 23, 2016, 9:13 PM
- */
+/******************************************************
+ ** FILE: CDataSortLast.h
+ **
+ ** ABSTRACT:
+ ** Sort database by last name
+ **
+ **
+ ** AUTHOR: Ezekiel Zandbergen, Cynthia Obia
+ **
+ ** CREATION DATE: 2-23-16
+ **
+ *******************************************************/
 
 #ifndef CDATASORTLAST_H
 #define CDATASORTLAST_H
